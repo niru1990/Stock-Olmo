cc5d126577267ce20020d8f381c29aeae1b58039
Moses Levy <moses.levy@gmail.com>
2019-09-26 22:34:21 -0300

    modifique titulo de pagin vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hola Grupo!!!! Bienvenidos a Stock Olmo!"/>
   </div>
 </template>
 
