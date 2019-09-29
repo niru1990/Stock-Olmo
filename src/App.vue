fcb2b3cb83ed9ef9ec555591bbf744fd62a54e47
niru1990 <nicolas.reyes90@gmail.com>
2019-09-29 15:34:23 -0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Bienvendio a Stock - Olmo el mejor sistema para manejar stock."/>
+    <div style="height:288px; background-color:#00FFFF	">
+      <div style="float:left">
+        <img alt="Vue logo" src="./assets/logo.png">
+      </div>
+      <div style="float:left; margin-top:50px; width:1000px; ">
+        <HelloWorld msg="Bienvendio a Stock - Olmo!"/>
+        <HelloWorld msg2="El mejor sistema para manejar tu stock."/>
+      </div>
+    </div>
+    <div style="float:left">
+      <dropdown/>
+    </div>
+    <div id="formularios">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from './components/HelloWorld.vue';
+import dropdown from './components/dropdown.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    dropdown
   }
 }
 </script>
